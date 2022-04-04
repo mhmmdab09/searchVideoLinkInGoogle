d316a6d675ad45affb1039d7e6d3433a4715b46c
mhmmdab09 <93433899+mhmmdab09@users.noreply.github.com>
2022-04-04 17:34:18 +0430

    add the main code
    start development here

---
+++ scrapper.py
@@ -0,0 +1,7 @@
+from googlesearch import search
+
+searchName = input("please insert the name : ")
+query = searchName + " site:sgegrv8ergs4sd555dsfa6bv8ajfg545.xyz"
+
+for i in search(query, num=10, start=0, stop=30, pause=2):
+    print(i)
