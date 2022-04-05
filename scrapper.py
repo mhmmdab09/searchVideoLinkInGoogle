19ef3fa9e389e44e916aef64497714e1d3eb6629
Mohammad Abtahi <smabtahi92@gmail.com>
2022-04-05 19:36:48 +0430

    editted search box

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,6 @@
 from googlesearch import search
 
-searchName = input("please insert the name : ")
+searchName = input("please insert your search : ")
 query = searchName + " site:sgegrv8ergs4sd555dsfa6bv8ajfg545.xyz"
 
 for i in search(query, num=10, start=0, stop=30, pause=2):
