d16ba07a649c1e6932a2f902c1c691f3a18459e6
Mohammad Abtahi <smabtahi922gmail.com>
2022-08-16 16:42:45 +0430

    final push

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,12 @@
 from googlesearch import search
 
 searchName = input("please insert search title : ")
-query = searchName + " site:sgegrv8ergs4sd555dsfa6bv8ajfg545.xyz"
 
-for i in search(query, num=10, start=0, stop=30, pause=2):
-    print(i)
+sites = ["sgegrv8ergs4sd555dsfa6bv8ajfg545.xyz", "dl2.sermovie.xyz"]
+
+queries = []
+for siteName in sites:
+    queries.append(searchName + " site:" + siteName)
+for m in queries:
+  for i in search(m, num=10, start=0, stop=30, pause=5):
+        print(i)
